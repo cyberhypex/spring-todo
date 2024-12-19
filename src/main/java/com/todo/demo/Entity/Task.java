3d01e414792fc21bb2a8a229c0156507ca0b6a6b
Anshuman <chaoanxuman999@gmail.com>
2024-12-19 12:01:23 +0530

    Task test done, thymeleaf working fine

--- src/main/java/com/todo/demo/Entity/Task.java
+++ src/main/java/com/todo/demo/Entity/Task.java
@@ -2,17 +2,43 @@ package com.todo.demo.Entity;
 
 
 import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
-@lombok.Data
+
 @Table(name="Task")
 public class Task {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long ID;
-    private String task;
+    private String title;
     private boolean hasCompleted;
 
 
+    public Long getID() {
+        return ID;
+    }
+
+    public void setID(Long ID) {
+        this.ID = ID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isHasCompleted() {
+        return hasCompleted;
+    }
+
+    public void setHasCompleted(boolean hasCompleted) {
+        this.hasCompleted = hasCompleted;
+    }
 }
